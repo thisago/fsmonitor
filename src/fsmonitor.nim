cb4bc0247f125043f6b14323b78e5d2f713fb1f8
Thiago Navarro <thiago@oxyoy.com>
2021-08-28 00:08:58 +0000

    Fixed some bugs; db dir is on home

--- src/fsmonitor.nim
+++ src/fsmonitor.nim
@@ -1,18 +1,17 @@
 #[
   Created at: 08/27/2021 20:23:54 Friday
-  Modified at: 08/27/2021 09:52:38 PM Friday
+  Modified at: 08/28/2021 12:08:01 AM Saturday
 ]#
 
-from std/os import commandLineParams, walkDirRec, fileExists, expandFilename
+from std/os import commandLineParams, walkDirRec, fileExists, expandFilename,
+                   getHomeDir, `/`, dirExists, createDir
 from std/sha1 import secureHashFile, `$`
 from std/tables import `[]=`, Table, pairs, hasKey, `[]`
 from std/json import parseJson, `{}`, to, `%*`, `%`, `[]=`, `$`
 from std/strformat import fmt
-from std/strutils import join
+from std/strutils import join, contains
 
-# from std/tables import `$`, pairs
-
-{.experimental: "codeReordering".}
+const dbFolder = getHomeDir() / ".fsmonitor"
 
 type
   Hashes* = Table[string, string]
@@ -20,23 +19,27 @@ type
     CkCreated, CkDeleted, CkEdited
   ChangedHashes* = Table[string, ChangeKind]
 
-proc getLastHashes(dbPath: string): Hashes =
+proc getLastHashes(dir: string; dbPath: string): Hashes =
   try:
-    let json = dbPath.readFile.parseJson
-    result = json.to Hashes
+    let
+      json = dbPath.readFile.parseJson
+      hashes = json.to Hashes
+    for (path, hash) in hashes.pairs:
+      if dir in path:
+        result[path] = hash
   except:
     quit "Error on parse DB"
 
 proc compare(newHashes, oldHashes: Hashes): ChangedHashes =
-  for (key, hash) in newHashes.pairs:
-    if oldHashes.hasKey key:
-      if hash != oldHashes[key]:
-        result[key] = CkEdited
+  for (path, hash) in newHashes.pairs:
+    if oldHashes.hasKey path:
+      if hash != oldHashes[path]:
+        result[path] = CkEdited
     else:
-      result[key] = CkCreated
-  for (key, hash) in oldHashes.pairs:
-    if not newHashes.hasKey key:
-      result[key] = CkDeleted
+      result[path] = CkCreated
+  for (path, hash) in oldHashes.pairs:
+    if not newHashes.hasKey path:
+      result[path] = CkDeleted
 
 proc `$`(self: Changekind): string =
   case self:
@@ -50,17 +53,23 @@ proc `$`(self: ChangedHashes): string =
     res.add fmt"{file}: {change}"
   result = res.join "\n"
 
-proc main*(dirPath: seq[string]; dbPath: string; genDb = false) =
+proc main*(dirPath: seq[string]; dbName = "db.json"; genDb = false) =
+  if not dirExists dbFolder:
+    createDir dbFolder
+  let
+    dbPath = dbFolder / dbName
+    dir = expandFilename dirPath[0]
   if dirPath.len == 1:
     var hashes: Hashes
-    for dir in dirPath[0].walkDirRec(relative = false):
+    for dir in dir.walkDirRec:
       hashes[expandFilename dir] = $secureHashFile dir
     if genDb:
       writeFile dbPath, $ %* hashes
     else:
       if dbPath.fileExists:
-        let lastHashes = dbPath.getLastHashes
+        let lastHashes = dir.getLastHashes dbPath
         echo hashes.compare(lastHashes)
+        # echo $lastHashes
   else:
     quit "Please provide just 1 parameter"
 
@@ -68,6 +77,6 @@ when isMainModule:
   import cligen
   dispatch(main, help = {
     "dirPath": "The path of directory to scan",
-    "dbPath": "The path of database",
+    "dbName": "The name of database (will be stored in ~/.fsmonitor/)",
     "genDb": "Regenerate the database?",
   })
